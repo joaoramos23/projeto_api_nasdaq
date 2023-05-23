44fb1873b5b2edd6c035d9b392c9935d8dbc1ad9
João Ramos <joao.fernando.ramos23@gmail.com>
2023-05-22 21:11:08 -0300

    alteracoes

--- api_bolsa_valores.py
+++ api_bolsa_valores.py
@@ -1,30 +1,69 @@
-import kaggle
-import zipfile
-import json
-import csv
 
+"""
+Comando para download da pasta .zip: 'kaggle datasets download -d paultimothymooney/stock-market-data'
+"""
+from zipfile import ZipFile
+import os
+import pandas as pd
+import glob
+import psycopg2
+
+def connect_db():  # conexão com o banco.
+    conn = psycopg2.connect(
+        host="localhost",
+        database="nasdaq_csv",
+        user="postgres",
+        password="klo5s871")
+    conn.autocommit=True
+    if conn.closed == 0:
+        print("Conexão estabelecida.")
+    else:
+        print("A conexão está fechada.")
+    return conn
+
+def create_cursor():
+    conn = connect_db()
+    cursor = conn.cursor()  # criando cursor para executar no banco.
+    return cursor
 
 def extract_csv_files():
-    z = zipfile.ZipFile('stock-market-data.zip', 'r')
-    for arquivo in z.namelist():
-        if 'nasdaq/csv' in arquivo:
-            z.extract(arquivo, 'nasdaq_csv')
-    z.close()
-
+    with ZipFile('stock-market-data.zip', 'r') as z:
+        for file in z.namelist():
+            if 'nasdaq/csv' in file:
+                z.extract(member=file, path='nasdaq_csv')
 
 def extract_json_files():
-    z = zipfile.ZipFile('stock-market-data.zip', 'r')
-    for arquivo in z.namelist():
-        if 'nasdaq/json' in arquivo:
-            z.extract(arquivo, 'nasdaq_json')
-    z.close()
+    with ZipFile('stock-market-data.zip', 'r') as zip_file:
+        for file in zip_file.namelist():
+            if 'nasdaq/json' in file:
+                zip_file.extract(file, path='nasdaq_json')
 
-with open('AAL.csv', "r") as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',')
-    for i, row in enumerate(spamreader):
-        if i == 0:
-            print('Cabecalho: ' + str(row))
-        else:
-            print('Valor: ' + str(row))
+def create_table(cursor):
+    cursor.execute("DROP TABLE IF EXISTS mercado_financeiro;CREATE TABLE mercado_financeiro (Id BIGSERIAL primary key,Id_Market INT,Name_Market VARCHAR(10),Date DATE,Low FLOAT,Open FLOAT,Volume FLOAT,High FLOAT,Close FLOAT,Adjusted_Close FLOAT);")
 
-#utilizar pandas
+def read_insert_csv(cursor):
+    #listar todos os csv nessa pasta.
+    csv_files = glob.glob(
+        r'C:\Users\jofin\Desktop\PROJETOS\Projeto API Bolsa de Valores\nasdaq_csv\stock_market_data\nasdaq\csv\*.csv')
+    id_market = 1
+    for file in csv_files:
+        try:
+            df = pd.read_csv(file)
+            if not df.empty:
+                name_file = os.path.basename(file)[:-4]
+                for _, row in df.iterrows():
+                    cursor.execute("INSERT INTO mercado_financeiro (Id_Market, Name_Market, Date, Low, Open, Volume, High, Close, Adjusted_Close) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);", (id_market, name_file, row['Date'], row['Low'], row['Open'], row['Volume'], row['High'], row['Close'], row['Adjusted Close']))
+                id_market += 1
+        except pd.errors.EmptyDataError:
+            print('Pulando pasta com erro.')
+        except pd.errors.ParserError:
+            print('Pulando pasta com erro.')
+
+def main_fuction():
+    #extract_csv_files()
+    #extract_json_files()
+    cursor = create_cursor()
+    create_table(cursor)
+    read_insert_csv(cursor)
+
+main_fuction()
